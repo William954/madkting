e8186dc376f843003cf1006cf0b2951402588d82
William Colin <williamcolin954@gmail.com>
2020-04-30 14:47:49 -0500

    .

---
+++ madkting/__init__.py
@@ -0,0 +1,11 @@
+# -*- coding: utf-8 -*-
+
+from . import controllers
+from . import models
+
+__author__ = 'Israel Calderón Aguilar'
+__copyright__ = '(C) 2019 All rights reserved by Madkting'
+__version__ = '12.0'
+__maintainer__ = 'Israel Calderón Aguilar'
+__email__ = 'israel@madkting.com'
+__status__ = 'Development'
---
+++ madkting/__manifest__.py
@@ -0,0 +1,42 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "madkting",
+
+    'summary': """
+        Integration with Madkting platform""",
+
+    'description': """
+        Module integration with madkting software platform.
+        - Create orders into your odoo software from marketplaces like Mercado Libre, Amazon, etc..
+        - Create products from madkting platform into odoo
+        - Update your stock from odoo to your madkting account.
+    """,
+
+    'author': "Israel Calderón Aguilar",
+    'website': "http://madkting.com/",
+
+    # Categories can be used to filter modules in modules listing
+    # Check https://github.com/odoo/odoo/blob/12.0/odoo/addons/base/data/ir_module_category_data.xml
+    # for the full list
+    'category': 'Sales',
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': [
+        'base',
+        'sale_management',
+        'stock',
+        'component_event'
+    ],
+    # always loaded
+    'data': [
+        'security/security.xml',
+        'security/ir.model.access.csv',
+        'views/views.xml',
+        # 'views/templates.xml',
+    ],
+    # only loaded in demonstration mode
+    'demo': [
+        # 'demo/demo.xml',
+    ],
+}
